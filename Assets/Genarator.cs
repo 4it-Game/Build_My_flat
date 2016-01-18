bac6327a24b0ad1839f53428d3ca68be17a3ecdb
thusitha madusanka <thusitha madusanka>
2016-01-18 11:40:36 +0530

    add Play again

---
+++ Assets/Genarator.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Genarator : MonoBehaviour
+{
+
+	public GameObject Spawn;
+	public float SpawnTime = 3;
+	[HideInInspector]public GameObject SelectedObject;
+	public float Speed = 3;
+
+	public bool lost = false;
+
+	void Start ()
+	{
+		StartCoroutine (WaitandSpawn (SpawnTime));
+	}
+
+
+	void Update ()
+	{
+		if (lost == false) {
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				SelectedObject.transform.Translate (Vector3.right * Speed * Time.deltaTime);
+			}
+
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				SelectedObject.transform.Translate (Vector3.left * Speed * Time.deltaTime);
+			}			
+		}
+	}
+
+	IEnumerator WaitandSpawn (float WaitTime)
+	{
+		yield return new WaitForSeconds (WaitTime);
+
+		float posX = Mathf.Round (Random.Range (-gameObject.transform.localScale.x / 2, gameObject.transform.localScale.x / 2));
+		Vector3 SpawnPos = new Vector3 (posX, gameObject.transform.position.y, gameObject.transform.position.z);
+		GameObject Cube = Instantiate (Spawn, SpawnPos, transform.rotation) as GameObject;
+		Cube.GetComponent<Rigidbody> ().drag = 4;
+		Cube.transform.localScale = new Vector3 (Random.Range (1, 3), Random.Range (1, 3), 1);
+		Cube.AddComponent<AddPoints> ();
+		Cube.tag = "Active";
+		if (SelectedObject != null && SelectedObject.tag == "Active") {
+			lost = true;
+		} 
+		SelectedObject = Cube;	
+		if (lost == false)
+			StartCoroutine (WaitandSpawn (SpawnTime));
+	}
+}
---
+++ Assets/Stats.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour
+{
+
+	public int Points = 0;
+	public GameObject Panel;
+	public Genarator _Gen;
+
+	void Update ()
+	{
+		Panel.SetActive (_Gen.lost);
+	}
+
+	public void PlayAgain ()
+	{
+		Application.LoadLevel ("game");
+	}
+}
